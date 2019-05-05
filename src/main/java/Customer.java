d52252c5b406a9c3e1aa45b8b461cd3c87432d8e
SlaxXxX <Janis.Schneider97@web.de>
2019-05-05 23:30:05 +0200

    added htmlStatement method

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -40,6 +40,25 @@ class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration rentalsEnum = rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></ H1 ><P >\n ";
+        while (rentalsEnum.hasMoreElements()) {
+            Rental each = (Rental) rentalsEnum.nextElement();
+            //show figures for each rental
+            result += each.getMovie().getTitle() + ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        //add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) +
+                "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+        return result;
+    }
+
+
     private double getTotalCharge() {
         double result = 0;
         Enumeration rentalsEnum = rentals.elements();
